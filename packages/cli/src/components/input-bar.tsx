30a7e194f45014eaf391042aa2925c134ed1e1b8
rudranshgoel1 <app.whatsupleads@gmail.com>
2026-07-22 20:59:41 +0530

    minor ui fix

--- packages/cli/src/components/input-bar.tsx
+++ packages/cli/src/components/input-bar.tsx
@@ -80,7 +80,7 @@ export function InputBar({ onSubmit, disabled = false }: Props) {
         } else {
             textarea.insertText(command.value + " ");
         }
-    }, [renderer, toast]);
+    }, [renderer, toast, dialog]);
 
     const handleCommandExecute = useCallback(
         (index: number) => {
